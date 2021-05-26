31a29738dcfe13bf38c7ecd779f31287dfd8daa0
Tadas <ledesp@fractal.lt>
2021-05-26 23:55:24 +0300

    tweaked to work with any coco folder

--- MaskRCNN/dataset.py
+++ MaskRCNN/dataset.py
@@ -6,6 +6,7 @@
 import numpy as np
 import os
 import tqdm
+
 try:
     import ujson as json
 except ImportError:
@@ -20,29 +21,20 @@ __all__ = ['COCODetection', 'DetectionDataset']
 
 
 class COCODetection(object):
-    # handle the weird (but standard) split of train and val
-    _INSTANCE_TO_BASEDIR = {
-        'valminusminival2014': 'val2014',
-        'minival2014': 'val2014',
-    }
-
-    COCO_id_to_category_id = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 11: 11, 13: 12, 14: 13, 15: 14, 16: 15, 17: 16, 18: 17, 19: 18, 20: 19, 21: 20, 22: 21, 23: 22, 24: 23, 25: 24, 27: 25, 28: 26, 31: 27, 32: 28, 33: 29, 34: 30, 35: 31, 36: 32, 37: 33, 38: 34, 39: 35, 40: 36, 41: 37, 42: 38, 43: 39, 44: 40, 46: 41, 47: 42, 48: 43, 49: 44, 50: 45, 51: 46, 52: 47, 53: 48, 54: 49, 55: 50, 56: 51, 57: 52, 58: 53, 59: 54, 60: 55, 61: 56, 62: 57, 63: 58, 64: 59, 65: 60, 67: 61, 70: 62, 72: 63, 73: 64, 74: 65, 75: 66, 76: 67, 77: 68, 78: 69, 79: 70, 80: 71, 81: 72, 82: 73, 84: 74, 85: 75, 86: 76, 87: 77, 88: 78, 89: 79, 90: 80}  # noqa
-    """
-    Mapping from the incontinuous COCO category id to an id in [1, #category]
-    For your own dataset, this should usually be an identity mapping.
-    """
-
-    # 80 names for COCO
-    class_names = [
-        "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]  # noqa
+    class_names = ['class1', 'class2', 'class3', 'class4']
 
     def __init__(self, basedir, name):
         self.name = name
-        self._imgdir = os.path.realpath(os.path.join(
-            basedir, self._INSTANCE_TO_BASEDIR.get(name, name)))
+        self._imgdir = os.path.realpath(os.path.join(basedir, name))
+
+        if not os.path.isdir(self._imgdir):
+            logger.warn(f'COCODetection: falling back to coco_{name} because {self._imgdir} is not a dir')
+            self._imgdir = os.path.realpath(os.path.join(basedir, f'coco_{name}'))
+
+        logger.info(f'COCODetection: image folder {name}')
         assert os.path.isdir(self._imgdir), self._imgdir
         annotation_file = os.path.join(
-            basedir, 'annotations/instances_{}.json'.format(name))
+            basedir, 'annotations/instances_{}.json'.format(name.replace('coco_', '')))
         assert os.path.isfile(annotation_file), annotation_file
 
         from pycocotools.coco import COCO
@@ -79,7 +71,6 @@ class COCODetection(object):
 
         return ret
 
-
     def load(self, add_gt=True, add_mask=False):
         """
         Args:
@@ -109,6 +100,73 @@ class COCODetection(object):
             return imgs
 
     def _add_detection_gt(self, img, add_mask):
+        """
+        Add 'boxes', 'class', 'is_crowd' of this image to the dict, used by detection.
+        If add_mask is True, also add 'segmentation' in coco poly format.
+        """
+        # ann_ids = self.coco.getAnnIds(imgIds=img['image_id'])
+        # objs = self.coco.loadAnns(ann_ids)
+        objs = self.coco.imgToAnns[img['image_id']]  # equivalent but faster than the above two lines
+
+        # clean-up boxes
+        width = img.pop('width')
+        height = img.pop('height')
+
+        all_boxes = []
+        all_segm = []
+        all_cls = []
+        all_iscrowd = []
+        for objid, obj in enumerate(objs):
+            if obj.get('ignore', 0) == 1:
+                continue
+            x1, y1, w, h = list(map(float, obj['bbox']))
+            # bbox is originally in float
+            # x1/y1 means upper-left corner and w/h means true w/h. This can be verified by segmentation pixels.
+            # But we do make an assumption here that (0.0, 0.0) is upper-left corner of the first pixel
+            x2, y2 = x1 + w, y1 + h
+
+            # np.clip would be quite slow here
+            x1 = min(max(x1, 0), width)
+            x2 = min(max(x2, 0), width)
+            y1 = min(max(y1, 0), height)
+            y2 = min(max(y2, 0), height)
+            w, h = x2 - x1, y2 - y1
+            # Require non-zero seg area and more than 1x1 box size
+            if obj['area'] > 1 and w > 0 and h > 0:
+                all_boxes.append([x1, y1, x2, y2])
+                # all_cls.append(self.COCO_id_to_category_id.get(obj['category_id'], obj['category_id']))
+                all_cls.append(obj['category_id'])
+                iscrowd = obj.get("iscrowd", 0)
+                all_iscrowd.append(iscrowd)
+
+                if add_mask:
+                    segs = obj['segmentation']
+                    if not isinstance(segs, list):
+                        assert iscrowd == 1
+                        all_segm.append(None)
+                    else:
+                        valid_segs = [np.asarray(p).reshape(-1, 2).astype('float32') for p in segs if len(p) >= 6]
+                        if len(valid_segs) == 0:
+                            logger.error("Object {} in image {} has no valid polygons!".format(objid, img['file_name']))
+                        elif len(valid_segs) < len(segs):
+                            logger.warn("Object {} in image {} has invalid polygons!".format(objid, img['file_name']))
+                        all_segm.append(valid_segs)
+
+        # all geometrically-valid boxes are returned
+        if len(all_boxes):
+            img['boxes'] = np.asarray(all_boxes, dtype='float32')  # (n, 4)
+        else:
+            img['boxes'] = np.zeros((0, 4), dtype='float32')
+        cls = np.asarray(all_cls, dtype='int32')  # (n,)
+        if len(cls):
+            assert cls.min() > 0, "Category id in COCO format must > 0!"
+        img['class'] = cls  # n, always >0
+        img['is_crowd'] = np.asarray(all_iscrowd, dtype='int8')  # n,
+        if add_mask:
+            # also required to be float32
+            img['segmentation'] = all_segm
+
+    def _add_detection_gt_orig(self, img, add_mask):
         """
         Add 'boxes', 'class', 'is_crowd' of this image to the dict, used by detection.
         If add_mask is True, also add 'segmentation' in coco poly format.
@@ -144,7 +202,8 @@ class COCODetection(object):
             # Require non-zero seg area and more than 1x1 box size
             if obj['area'] > 1 and w > 0 and h > 0 and w * h >= 4:
                 all_boxes.append([x1, y1, x2, y2])
-                all_cls.append(self.COCO_id_to_category_id.get(obj['category_id'], obj['category_id']))
+                # all_cls.append(self.COCO_id_to_category_id.get(obj['category_id'], obj['category_id']))
+                all_boxes.append(obj['category_id'])
                 iscrowd = obj.get("iscrowd", 0)
                 all_iscrowd.append(iscrowd)
 
@@ -167,7 +226,7 @@ class COCODetection(object):
         cls = np.asarray(all_cls, dtype='int32')  # (n,)
         if len(cls):
             assert cls.min() > 0, "Category id in COCO format must > 0!"
-        img['class'] = cls          # n, always >0
+        img['class'] = cls  # n, always >0
         img['is_crowd'] = np.asarray(all_iscrowd, dtype='int8')  # n,
         if add_mask:
             # also required to be float32
@@ -195,6 +254,7 @@ class DetectionDataset(object):
 
     To use your own dataset that's not in COCO format, rewrite all methods of this class.
     """
+
     def __init__(self):
         """
         This function is responsible for setting the dataset-specific
@@ -248,7 +308,7 @@ class DetectionDataset(object):
             id (str): an id for the image. The inference results will be stored with this id.
         """
         return COCODetection.load_many(cfg.DATA.BASEDIR, name, add_gt=False)
-    
+
     def load_viz_roidbs(self, name):
         """
         Args:
@@ -283,13 +343,14 @@ class DetectionDataset(object):
         Returns:
             dict: the evaluation results.
         """
-        continuous_id_to_COCO_id = {v: k for k, v in COCODetection.COCO_id_to_category_id.items()}
+        # continuous_id_to_COCO_id = {v: k for k, v in COCODetection.COCO_id_to_category_id.items()}
+
         coco_results = {}
         coco_results['bbox'] = []
         coco_results['segm'] = []
         for res in results:
             # convert to COCO's incontinuous category id
-            res['category_id'] = continuous_id_to_COCO_id[res['category_id']]
+            # res['category_id'] = continuous_id_to_COCO_id[res['category_id']]
             # COCO expects results in xywh format
             box = res['bbox']
             box[2] -= box[0]
@@ -314,16 +375,17 @@ class DetectionDataset(object):
         assert output is not None and len(output) > 0, "COCO evaluation requires an output file!"
         coco = COCODetection(cfg.DATA.BASEDIR, dataset)
 
-        with open(output+'box.json', 'w') as f:
+        with open(output + 'box.json', 'w') as f:
             json.dump(coco_results["bbox"], f)
-        ret.update(coco.print_coco_metrics(output+'box.json', 'bbox'))
+        ret.update(coco.print_coco_metrics(output + 'box.json', 'bbox'))
 
         if len(coco_results['segm']):
-            with open(output+'segm.json', 'w') as f:
+            with open(output + 'segm.json', 'w') as f:
                 json.dump(coco_results['segm'], f)
-            ret.update(coco.print_coco_metrics(output+'segm.json', 'segm'))
+            ret.update(coco.print_coco_metrics(output + 'segm.json', 'segm'))
 
         return ret
+
     # code for singleton:
     _instance = None
 
@@ -334,6 +396,7 @@ class DetectionDataset(object):
 
 
 if __name__ == '__main__':
-    c = COCODetection(cfg.DATA.BASEDIR, 'train2014')
+    basedir = '/Users/tadas/temp/gasefis/data'
+    c = COCODetection(basedir, 'coco_ballets')
     gt_boxes = c.load(add_gt=True, add_mask=True)
     print("#Images:", len(gt_boxes))
